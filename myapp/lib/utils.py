37a7c96b9c72a42ebcf3cb76040c21848494dae4
Hiroyasu Shimizu <hryssmz@outlook.com>
2023-04-14 15:14:31 +0900

    update source

--- myapp/lib/utils.py
+++ myapp/lib/utils.py
@@ -3,4 +3,4 @@ from datetime import datetime
 
 def isoformat_now() -> str:
     dt_utc = datetime.utcnow()
-    return dt_utc.strftime("%Y-%m-%dT%H:%M:%S%Z")
+    return dt_utc.strftime("%Y-%m-%dT%H:%M:%SZ")
